74553cd3fa81cc3a59872a68d60d5236b8903c71
Shantharambaskar <shantharambaskar@gmail.com>
2026-07-17 10:05:17 +0530

    Initial commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // Central API configuration
 // Change this URL when deploying to production
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://api.sbmgroups.online';
 
 export default API_BASE_URL;
 
